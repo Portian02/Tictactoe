537933bbf500d44909cd9f12639cc6ccd72842c6
Sebastian Arias <sebasariasporras@gmail.com>
2023-06-28 13:36:29 -0600

    agregar cambios en js y CSS

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,15 +1,31 @@
-let isPlayerOne = true;
 let cells = document.getElementsByClassName("cell");
 
-for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("click", jugador);
-}
-function jugador(e) {
-  let cellvalue = e.target.innerHTML;
-  if (!cellvalue.length) {
-    e.target.innerHTML = "X" 
-   
+let movesAvaible = [];
 
+// Funcion jugador
+
+let limite = 9;
+let nuevaLista = [];
+for (let i = 0; i < limite; i++) {
+  let ramdomC = Math.floor(Math.random() * limite);
+  nuevaLista = cells[i].filter(" ");
+  cells[i].addEventListener("click", function () {
+    console.log(cells[i]);
+
+    cells[i].innerHTML = "🐱";
+
+    //validar que solomanete los circulos, oseala jugada de la maquina sea en campos vacios y no en X U O
+
+    setTimeout(() => {
+      cells[ramdomC].innerHTML = "🐭";
+      console.log(cells[ramdomC]);
+    }, 1000);
+  });
+}
+
+/*
+    console.log(cells);
+    isTurn();
     checkLine(0, 1, 2);
     checkLine(3, 4, 5);
     checkLine(6, 7, 8);
@@ -18,8 +34,28 @@ function jugador(e) {
     checkLine(2, 5, 8);
     checkLine(0, 4, 8);
     checkLine(6, 4, 2);
+
+   */
+
+/*
+
+function Computer(c) {
+  let cellC = c.target.innerHTML;
+  if (!cellC.length) {
+    cellC[ramdomC].filter(" ").c.innerHTML = "O";
   }
 }
+// FUNCIÓ TURNOS
+function isTurn() {
+  if (cells.value === "X" || cells.value !== "O") {
+    document.querySelector("#Turns").innerHTML = "Es turno <br> de:  O";
+  } else if (cells.value === "O" && cells.value !== "X") {
+    cells.value.filter("O");
+    document.querySelector("#Turns").innerHTML = "Es turno <br> de: X ";
+  }
+}
+
+// FUNCIÓN PARA CHECAR QUIÉN GANÓ
 function checkLine(c1, c2, c3) {
   if (
     cells[c1].innerHTML.length &&
@@ -29,11 +65,14 @@ function checkLine(c1, c2, c3) {
     showWinner(cells[c1].innerHTML);
   }
 }
-
+// FUNCIÓN MOSTRAR GANADOR
 function showWinner(player) {
   document.querySelector("#Resultado").innerHTML = player + " " + "Ganador!!!";
 }
+*/
 
+// FUNCIÓN BOTÓN DE RESTART
 function Reset() {
   location.reload();
 }
+function Stopgame(game) {}
