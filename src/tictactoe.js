78d73e269623de40ea68e245a075644674bc94a8
Sebastian Arias <sebasariasporras@gmail.com>
2023-06-29 11:43:41 -0600

    agregar actualización en la función game

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,29 +1,31 @@
 let cells = document.getElementsByClassName("cell");
+let limite = 9;
+let nuevaLista = [];
 
 // Funcion jugador
 
-let limite = 9;
-let nuevaLista = ["", "", "", "", "", "", "", "", ""];
 for (let i = 0; i < limite; i++) {
+  nuevaLista.push(i);
+
   cells[i].addEventListener("click", function () {
-    let filtro = nuevaLista.filter((vacío) => vacío.length < 1);
-    let espacios = filtro;
-    let ramdomC = Math.floor(Math.random() * limite);
-    console.log(cells[i]);
+    let toDelete = nuevaLista.findIndex((celda) => celda === i);
+    if (toDelete !== -1) {
+      cells[i].innerHTML = "🐱";
+      console.log(cells[i].innerHTML);
+      nuevaLista.splice(toDelete, 1);
+      let randomAvailable = Math.floor(Math.random() * nuevaLista.length);
+      let index = nuevaLista[randomAvailable];
 
-    cells[i].innerHTML = "🐱";
+      //validar que solomanete los circulos, oseala jugada de la maquina sea en campos vacios y no en X U O
+      // cells[i] = nuevaLista.filter((vacío) => vacío.length < 1);
 
-    //validar que solomanete los circulos, oseala jugada de la maquina sea en campos vacios y no en X U O
+      setTimeout(() => {
+        cells[index].innerHTML = "🐭";
+        console.log(cells[index].innerHTML);
+        nuevaLista.splice(randomAvailable, 1);
+      }, 1000);
+    }
 
-    setTimeout(() => {
-      cells[ramdomC].innerHTML = "🐭";
-      console.log(cells[ramdomC]);
-    }, 1000);
-  });
-}
-
-/*
-    console.log(cells);
     isTurn();
     checkLine(0, 1, 2);
     checkLine(3, 4, 5);
@@ -33,18 +35,11 @@ for (let i = 0; i < limite; i++) {
     checkLine(2, 5, 8);
     checkLine(0, 4, 8);
     checkLine(6, 4, 2);
-
-   */
-
-/*
-
-function Computer(c) {
-  let cellC = c.target.innerHTML;
-  if (!cellC.length) {
-    cellC[ramdomC].filter(" ").c.innerHTML = "O";
-  }
+  });
 }
 // FUNCIÓ TURNOS
+
+
 function isTurn() {
   if (cells.value === "X" || cells.value !== "O") {
     document.querySelector("#Turns").innerHTML = "Es turno <br> de:  O";
@@ -60,17 +55,39 @@ function checkLine(c1, c2, c3) {
     cells[c1].innerHTML.length &&
     cells[c1].innerHTML === cells[c2].innerHTML &&
     cells[c2].innerHTML === cells[c3].innerHTML
-  ) {
-    showWinner(cells[c1].innerHTML);
+    ) {
+      showWinner(cells[c1].innerHTML);
+    }
   }
-}
-// FUNCIÓN MOSTRAR GANADOR
-function showWinner(player) {
-  document.querySelector("#Resultado").innerHTML = player + " " + "Ganador!!!";
-}
-*/
 
-// FUNCIÓN BOTÓN DE RESTART
-function Reset() {
-  location.reload();
+function endGame() {
+  
 }
+
+
+
+
+
+  // FUNCIÓN MOSTRAR GANADOR
+  function showWinner(player) {
+    document.querySelector("#Resultado").innerHTML = player + " " + "Ganador!!!";
+  }
+  
+  
+  // FUNCIÓN BOTÓN DE RESTART
+  function Reset() {
+    location.reload();
+  }
+  /*
+      console.log(cells);
+      isTurn();
+      checkLine(0, 1, 2);
+      checkLine(3, 4, 5);
+      checkLine(6, 7, 8);
+      checkLine(0, 3, 6);
+      checkLine(1, 4, 7);
+      checkLine(2, 5, 8);
+      checkLine(0, 4, 8);
+      checkLine(6, 4, 2);
+  
+     */
